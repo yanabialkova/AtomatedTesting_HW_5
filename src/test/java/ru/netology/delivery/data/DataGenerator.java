223814fdb69236cbbf8c59c0885f084fa94d3e72
Yana Bialkova <yana.bialkova@gmail.com>
2022-08-24 18:19:20 +0200

    Initial comit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,55 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    static Faker faker;
+
+    @BeforeEach
+    void setUpAll () {
+        faker = new Faker(new Locale("ru"));
+    }
+
+    public static String generateDate(int shift) {
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity(String locale) {
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
